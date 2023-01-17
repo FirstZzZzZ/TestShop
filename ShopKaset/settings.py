79c9bbfa2c7dd3ca18d9063657d43dde7a57e2c9
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-17 13:10:31 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -142,11 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-BASE_DIR.joinpath('static')
-]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 
 # Default primary key field type
