ef8f66e6c7a0c7603e22b6326caa5ad733d512c5
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-17 11:39:18 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 #keep the secret key used in production secret
-SECRET_KEY = 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 
 #don't run with debug turned on in production
 DEBUG = os.environ.get('DEBUG',true)
