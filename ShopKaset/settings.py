f0a22bd1ad718b91b1bc5c8f30be297af14fc64e
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-17 13:04:45 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from email.mime import base
 import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
