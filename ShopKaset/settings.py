0a54097c1d3a1512230942f230533fd1a53690ae
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-18 14:03:50 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
 BASE_DIR.joinpath('static')
 ]
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
