746943ca44f591cb999669f317fc2726f9dee20d
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-19 11:13:27 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from email.mime import base
 import os
+import sys
+import dj_database_url
 
 from dotenv import load_dotenv
 from pathlib import Path
