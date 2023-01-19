8a4abeaefd222640d57e3337ed464cd0899c22ba
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-19 11:16:39 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -95,19 +95,34 @@ WSGI_APPLICATION = 'ShopKaset.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storedb_kaset',
-        'USER': 'root',
-        'PASSWORD' :'',
-        'HOST':'localhost',
-        'PORT':'',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
-        },
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.mysql',
+      #  'NAME': 'storedb_kaset',
+       # 'USER': 'root',
+       # 'PASSWORD' :'',
+        #'HOST':'localhost',
+        #'PORT':'',
+        #'OPTIONS': {
+         #   'sql_mode': 'STRICT_TRANS_TABLES',
+        #},
+   #}
+#}
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
