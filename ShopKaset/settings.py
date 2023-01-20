f54921bf119f77f2b8edd02a4a5750ebdbfe7fbc
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-20 10:40:41 +0700

    Update settings.py

--- ShopKaset/settings.py
+++ ShopKaset/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'ShopKaset.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'Template')],
+        'DIRS': [os.path.join(BASE_DIR,'store01/Templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
