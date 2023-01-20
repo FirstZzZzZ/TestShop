27629b2ad26a5107acfd2f2449f83fb89111b225
FirstZzZzZ <60383233+FirstZzZzZ@users.noreply.github.com>
2023-01-20 15:21:19 +0700

    Update admin.py

--- store01/admin.py
+++ store01/admin.py
@@ -88,5 +88,5 @@ admin.site.register(OrderItem,OrderItemAdmin,)
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+admin.site.register(User)
+admin.site.register(Group)
